98f5141be7f55da940bc1785a49a357baf06d330
Andrew <42828962+Lepieu@users.noreply.github.com>
2024-07-15 09:52:23 -0400

    Add files via upload

---
+++ photorecognition.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+
+"""
+Returns an 
+"""
+def OpenImage():
+    #TODO: Impliment this method --> open image file using openCV 
+    return
+
+"""
+Returns a filtered version of the input image using the input filter. By default, it will use the 
+"""
+def Filter(img, filter=[1, 1, 1, 1, 8, 1, 1, 1, 1]): 
+    #TODO: use openCV to apply a pixel filter
+    return
+
+"""
+Returns 
+"""
+def Pooling():
+    #TODO: probably impliment max pool
+    return
+
+"""
+Returns the compiled neural network 
+"""
+def Training():
+    #TODO: Prepare and feed it into a neural network
+    return
+
+def Recognition():
+    #TODO: all of it
+    return
